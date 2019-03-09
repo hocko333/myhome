8db12bf824cbdf0adba0ea6af5b85242b2ad7030
chengkuan <15927614969@163.com>
2019-03-09 11:52:56 +0800

    semantic-ui配置完成

---
+++ src/login.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './login.css'
+import { Button } from 'semantic-ui-react'
+
+class Login extends React.Component {
+  // constructor(props) {
+  //   super(props)
+  // }
+  render() {
+    return (
+      <div>
+        <Button>登录</Button>
+      </div>
+    )
+  }
+}
+
+export default Login
